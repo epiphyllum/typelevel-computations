6741619bf5c930c4b41bb0ea2d80cc971e6f237b
George Leontiev <folone@gmail.com>
2014-01-14 14:05:44 +0100

    Example.

--- core/src/main/scala/test.scala
+++ core/src/main/scala/test.scala
@@ -17,4 +17,21 @@ object Test {
       gcd(Y, X, Out)
     }
   }
+  /*
+   * Should generate the following code:
+   * trait GCD[X <: Nat, Y <: Nat, Z <: Nat] {}
+   *
+   * object GCD {
+   *   implicit def gcd0[X <: Nat]: GCD[X, X, X] =
+   *     new GCD[X, X, X] {}
+   *   implicit def gcd1[X <: Nat, Y <: Nat, Z <: Nat, Out <: Nat]
+   *     (implicit ev0 : LT[X, Y], ev1 : Diff.Aux[Y, X, Z],
+   *               ev2 : GCD[X, Z, Out]): GCD[X, Y, Out] =
+   *       new GCD[X, Y, Out] {}
+   *   implicit def gcd2[X <: Nat, Y <: Nat, Out <: Nat]
+   *     (implicit ev0 : LT[Y, X],
+   *               ev1 : GCD[Y, X, Out]): GCD[X, Y, Out] =
+   *     new GCD[X, Y, Out] {}
+   * }
+   */
 }
